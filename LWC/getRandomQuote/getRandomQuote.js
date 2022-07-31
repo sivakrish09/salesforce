44260e93064b76f983bf24d7dda1e4bec020beee
Sivasubramaniyan Krishanmoorthi <68295498+sivakrish09@users.noreply.github.com>
2022-07-31 14:24:44 +0530

    Update getRandomQuote.js

--- LWC/getRandomQuote/getRandomQuote.js
+++ LWC/getRandomQuote/getRandomQuote.js
@@ -1,3 +1,19 @@
 import { LightningElement } from 'lwc';
+import getRandomQuote from '@salesforce/apex/dogRandomImage.getRandomQuote';
 
-export default class GetRandomQuote extends LightningElement {}
+export default class LwcRandomQuote extends LightningElement
+ {
+    message;
+    authors;
+    connectedCallback()
+    {
+        getRandomQuote({}).then(
+            (response)=>{
+                this.message = JSON.parse(response).quote;
+                this.authors = JSON.parse(response).author
+
+            }
+        );
+    }
+
+}
